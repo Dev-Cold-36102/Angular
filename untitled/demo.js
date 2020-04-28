6edd0ef3e25aca8200ff0615bb22cdd0fa0e3ad4
Dev.Cold <devcolduet@gmail.com>
2020-04-28 18:31:22 +0700

    add

---
+++ untitled/demo.js
@@ -0,0 +1,7 @@
+var list = [1, 2, 3];
+var newList = ["dsfds"];
+console.log(list);
+var listSquare = list.map(function (num) { return num * num; });
+console.log(listSquare);
+var newListFilter = list.filter(function (number) { return number > 1; });
+console.log(newListFilter);
---
+++ untitled/demo.ts
@@ -0,0 +1,7 @@
+const list: number[] = [1, 2, 3];
+const newList: Array<string> = ["dsfds"];
+console.log(list);
+const listSquare = list.map(num => num * num);
+console.log(listSquare);
+const newListFilter =list.filter(number=>number>1 );
+console.log(newListFilter)
